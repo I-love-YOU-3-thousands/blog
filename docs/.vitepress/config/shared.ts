d3fc9008cc1fbe5601622d5b775a9b20e80af662
safety <m18137070310@163.com>
2024-09-27 13:48:01 +0800

    first commit

---
+++ docs/.vitepress/config/shared.ts
@@ -0,0 +1,100 @@
+import { defineConfig } from "vitepress";
+import { search as enSearch } from "./en";
+import { search as zhSearch } from "./zh";
+export const shared = defineConfig({
+  title: "前端知识汇总",
+  base: "/blog/",
+  rewrites: {
+    "zh/:rest*": ":rest*",
+  },
+
+  lastUpdated: true,
+  cleanUrls: true, //开启纯净链接
+  metaChunk: true,
+
+  markdown: {
+    math: true,
+    image: {
+      // 开启图片懒加载
+      lazyLoading: true,
+    },
+    theme: "material-theme-palenight",
+    codeTransformers: [
+      // We use `[!!code` in demo to prevent transformation, here we revert it back.
+      {
+        postprocess(code) {
+          return code.replace(/\[\!\!code/g, "[!code");
+        },
+      },
+    ],
+  },
+
+  sitemap: {
+    hostname: "https://vitepress.dev",
+    transformItems(items) {
+      return items.filter((item) => !item.url.includes("migration"));
+    },
+  },
+
+  /* prettier-ignore */
+  head: [
+    ['link', { rel: 'icon', type: 'image/svg+xml', href: '/blog/logo.svg' }],
+    ['link', { rel: 'icon', type: 'image/jpg', href: '/blog/logo.jpg' }],
+    ['meta', { name: 'theme-color', content: '#5f67ee' }],
+    ['meta', { property: 'og:type', content: 'website' }],
+    ['meta', { property: 'og:locale', content: 'zh' }],
+    ['meta', { property: 'og:title', content: 'VitePress | Vite & Vue Powered Static Site Generator' }],
+    ['meta', { property: 'og:site_name', content: 'VitePress' }],
+    ['meta', { property: 'og:image', content: 'https://vitepress.dev/vitepress-og.jpg' }],
+    ['meta', { property: 'og:url', content: 'https://vitepress.dev/' }],
+    ['script', { src: 'https://cdn.usefathom.com/script.js', 'data-site': 'AZBRSFGG', 'data-spa': 'auto', defer: '' }],
+    [
+        'link',
+        { rel: 'preconnect', href: 'https://fonts.googleapis.com' }
+      ],
+      [
+        'link',
+        { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' }
+      ],
+      [
+        'link',
+        { href: 'https://fonts.googleapis.com/css2?family=Roboto&display=swap', rel: 'stylesheet' }
+      ]
+
+  ],
+  ignoreDeadLinks: true, //忽略死链
+
+  themeConfig: {
+    // logo: { src: "/logo.svg", width: 24, height: 24 },
+
+    socialLinks: [
+      {
+        icon: "github",
+        link: "https://github.com/I-love-YOU-3-thousands/blog",
+        ariaLabel: "github",
+      },
+      {
+        icon: {
+          svg: '<svg t="1727406569967" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="21231" width="200" height="200"><path d="M306.005333 117.632L444.330667 256h135.296l138.368-138.325333a42.666667 42.666667 0 1 1 60.373333 60.373333L700.330667 256H789.333333A149.333333 149.333333 0 0 1 938.666667 405.333333v341.333334a149.333333 149.333333 0 0 1-149.333334 149.333333h-554.666666A149.333333 149.333333 0 0 1 85.333333 746.666667v-341.333334A149.333333 149.333333 0 0 1 234.666667 256h88.96L245.632 177.962667a42.666667 42.666667 0 1 1 60.373333-60.373334v0.042667zM789.333333 341.333333h-554.666666a64 64 0 0 0-63.701334 57.856L170.666667 405.333333v341.333334a64 64 0 0 0 57.856 63.701333L234.666667 810.666667h554.666666a64 64 0 0 0 63.701334-57.856L853.333333 746.666667v-341.333334A64 64 0 0 0 789.333333 341.333333zM341.333333 469.333333a42.666667 42.666667 0 0 1 42.666667 42.666667v85.333333a42.666667 42.666667 0 1 1-85.333333 0v-85.333333a42.666667 42.666667 0 0 1 42.666666-42.666667z m341.333334 0a42.666667 42.666667 0 0 1 42.666666 42.666667v85.333333a42.666667 42.666667 0 0 1-85.333333 0v-85.333333a42.666667 42.666667 0 0 1 42.666667-42.666667z" fill="#09121F" p-id="21232"></path></svg>',
+        },
+        link: "https://space.bilibili.com/388661286",
+        ariaLabel: "bilibili",
+      },
+    ],
+
+    search: {
+      provider: "algolia",
+      options: {
+        appId: "8J64VVRP8K",
+        apiKey: "52f578a92b88ad6abde815aae2b0ad7c",
+        indexName: "vitepress",
+        locales: {
+          ...zhSearch,
+          ...enSearch,
+        },
+      },
+    },
+
+    carbonAds: { code: "CEBDT27Y", placement: "vuejsorg" },
+  },
+});
---
+++ docs/zh/index.md
@@ -0,0 +1,53 @@
+---
+layout: home
+
+hero:
+  # 主标题
+  name: Safety 博客
+  #   副标题
+  text: "VitePress + Vue 搭建"
+  #   内容介绍
+  tagline: 如果你也想搭建它，那跟我一起做吧
+  # 右边图片
+  image:
+    src: /卫星.png
+    alt: satellite
+  actions:
+    # 按钮主题
+    - theme: brand
+      # 按钮文字
+      text: 开始
+      link: /getting-started
+    - theme: alt
+      text: GitHub
+      link: https://github.com/I-love-YOU-3-thousands/blog
+    - theme: sponsor
+      text: 搭建导航
+      link: /nav/
+
+features:
+  - icon: ⚡️
+    # 标题
+    title: 前端知识
+    #  介绍
+    details: HTML CSS JavaScript TypeScript Vue3等部分的基础以及进阶的内容知识点,整合自己以及别人的资料
+  - icon: 🛠️
+    title: 常用前端工具
+    details: 提供一系列的提高前端开发效果的工具网站,例如UI渐变色生成器 盒子阴影调试 Flex Grid 布局调试 包括常用的图标库...
+  - icon:
+      dark: /vitepress.png
+      light: /vitepress-light.png
+    title: 享受Vite DX
+    details: 即时服务器启动，闪电般快速的热更新，并利用 Vite 生态插件。
+    link: https://vitejs.cn/
+    linkText: Vite
+  - icon: 🚀
+    title: 快速发布网站
+    details: 使用静态 HTML 进行快速初始加载，使用客户端路由进行快速加载后导航
+---
+
+<HomeUnderline />
+
+<confetti />
+
+<busuanzi />
