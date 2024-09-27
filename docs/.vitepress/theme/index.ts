25b55c00a25842c8b89b926093ddf1ee8061f94b
safety <m18137070310@163.com>
2024-09-27 15:21:46 +0800

    添加五彩纸屑

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,7 +1,14 @@
 import DefaultTheme from "vitepress/theme";
 import CommentLayout from "./components/CommentLayout.vue";
+import confetti from './components/confetti.vue'
 export default {
   extends: DefaultTheme,
   // 使用注入插槽的包装组件覆盖 Layout
   Layout: CommentLayout,
+  enhanceApp({ app }) {
+    app.component("confetti", confetti);
+    // // app.use(BgmPlayer.install);
+    // vitepressMusic(playlist);
+    // BgmPlayer;
+  },
 };
